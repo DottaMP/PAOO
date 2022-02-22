465d0810f69fb1f5253b134b09652b7ef174eb6d
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2022-02-22 14:13:54 -0300

    update

--- aula2/src/Exemplo1.ts
+++ aula2/src/Exemplo1.ts
@@ -2,8 +2,14 @@
 //Criar o json pelo comando tsc --init
 //tsconfig.json: o tsc "observa" o arquivo que fornece as opções de compilação.
 // outDir (js) é onde são depositadas os resultados do transpilação dos arquivos que estão em "rootDir" (ts).
-//Abrir o tsconfig.json e no outDir configurar para gerar todas as saídas colocando o ./dist 
-            // no rootDir colocar o ./src. Tirar o comentário deles.
+//Abrir o tsconfig.json e deixa da seguinte maneira: "outDir": "./dist",   | "rootDir": "./src", | no "target": "es2020", | Não esquecer de tirar os comentários deles.
+
+//Observações: //Sempre inserir o console.log
+//no terminal dentro da pasta do programa, colocar tsc nomeDoArquivo.ts
+//Será criado um arquivo de mesmo nome .js
+//no terminal colocar node nomeDoArquivo.js para executar o programa.
+//para compilar automaticamente usa-se no terminal o comando tsc -w para abrir o terminal node.
+
 
 // números inteiros "normais" de 64 bits    
 function sum(a: number, b: number): number{ //o tipo devolve um número, o devolve fica no final
@@ -33,9 +39,3 @@ function rfat(n: bigint): bigint{ //o tipo devolve um número, o devolve fica no
         :n * rfat(n-1n);
 }
 console.log(fat(50n)); // precisa colocar o n para falar que é bigint
-
-//Sempre inserir o console.log
-//no terminal dentro da pasta do programa, colocar tsc nomeDoArquivo.ts
-//Será criado um arquivo de mesmo nome .js
-//no terminal colocar node nomeDoArquivo.js para executar o programa.
-//para compilar automaticamente usa-se no terminal o comando tsc -w para abrir o terminal node.
