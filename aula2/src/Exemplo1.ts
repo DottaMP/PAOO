3eefba5e13ab85b1c989221ee73c7e05e406eb23
Mayara Dotta <67791520+DottaMP@users.noreply.github.com>
2022-02-22 14:29:13 -0300

    update

--- aula2/src/Exemplo1.ts
+++ aula2/src/Exemplo1.ts
@@ -2,7 +2,7 @@
 //Criar o json pelo comando tsc --init
 //tsconfig.json: o tsc "observa" o arquivo que fornece as opções de compilação.
 // outDir (js) é onde são depositadas os resultados do transpilação dos arquivos que estão em "rootDir" (ts).
-//Abrir o tsconfig.json e deixa da seguinte maneira: "outDir": "./dist",   | "rootDir": "./src", | no "target": "es2020", | Não esquecer de tirar os comentários deles.
+//Abrir o tsconfig.json e deixar da seguinte maneira: "outDir": "./dist",   | "rootDir": "./src", | no "target": "es2020", | Não esquecer de tirar os comentários deles.
 
 //Observações: //Sempre inserir o console.log
 //no terminal dentro da pasta do programa, colocar tsc nomeDoArquivo.ts
